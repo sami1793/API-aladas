c77a603a1696efc766b254fe6c0ecbc1585883ef
sami1793 <sami_17_94@hotmail.com>
2021-07-25 01:49:56 -0300

    Se crea entitie Usuario

---
+++ src/main/java/ar/com/ada/api/aladas/entities/Usuario.java
@@ -0,0 +1,170 @@
+package ar.com.ada.api.aladas.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usuario_id")
+    private Integer usuarioId;
+
+    
+    @NaturalId
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    @Column(name = "fecha_login")
+    private Date fechaLogin;
+
+    @Column(name = "tipo_usuario_id")
+    private Integer tipoUsuarioId;
+
+    @OneToOne
+    @JoinColumn(name = "staff_id", referencedColumnName = "staff_id")
+    private Staff staff;
+
+    @OneToOne
+    @JoinColumn(name = "pasajero_id", referencedColumnName = "pasajero_id")
+    private Pasajero pasajero;
+
+    
+    public Integer getUsuarioId() {
+        return usuarioId;
+    }
+
+
+
+    public void setUsuarioId(Integer usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+
+
+    public String getUsername() {
+        return username;
+    }
+
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
+    public String getPassword() {
+        return password;
+    }
+
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+    public Date getFechaLogin() {
+        return fechaLogin;
+    }
+
+
+
+    public void setFechaLogin(Date fechaLogin) {
+        this.fechaLogin = fechaLogin;
+    }
+
+
+
+    public Integer getTipoUsuarioId() {
+        return tipoUsuarioId;
+    }
+
+
+
+    public void setTipoUsuarioId(Integer tipoUsuarioId) {
+        this.tipoUsuarioId = tipoUsuarioId;
+    }
+
+
+
+    public Staff getStaff() {
+        return staff;
+    }
+
+
+
+    public void setStaff(Staff staff) {
+        this.staff = staff;
+    }
+
+
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
+
+
+
+    public void setPasajero(Pasajero pasajero) {
+        this.pasajero = pasajero;
+    }
+
+
+
+    public enum TipoUsuarioEnum {
+        STAFF(1), PASAJERO(2);
+
+        private final int value;
+
+        // NOTE: Enum constructor tiene que estar en privado
+        private TipoUsuarioEnum(Integer value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public static TipoUsuarioEnum parse(Integer id) {
+            TipoUsuarioEnum status = null; // Default
+            for (TipoUsuarioEnum item : TipoUsuarioEnum.values()) {
+                if (item.getValue() == id) {
+                    status = item;
+                    break;
+                }
+            }
+            return status;
+        }
+    }
+
+}
