116031e4b60e4ff73ca35ba029c7207bfe1d2a95
sami1793 <sami_17_94@hotmail.com>
2021-07-25 16:54:56 -0300

    Cree el edpoint "/api/aeropuertos" y u  GenericResponde en Models.

---
+++ src/main/java/ar/com/ada/api/aladas/controllers/AeropuertoController.java
@@ -0,0 +1,29 @@
+package ar.com.ada.api.aladas.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import ar.com.ada.api.aladas.entities.Aeropuerto;
+import ar.com.ada.api.aladas.models.response.GenericResponse;
+import ar.com.ada.api.aladas.services.AeropuertoServices;
+
+@RestController
+public class AeropuertoController {
+    @Autowired
+    AeropuertoServices service;
+
+    @PostMapping("/api/aeropuertos")
+    public ResponseEntity<GenericResponse> crear(@RequestBody Aeropuerto aeropuerto){
+
+        GenericResponse respuesta= new GenericResponse();
+
+        service.crear(aeropuerto.getAeropuertoId(), aeropuerto.getNombre(), aeropuerto.getCodigoIATA());
+
+        respuesta.isOk= true;
+        respuesta.message="Aeropuerto creado correctamente";
+
+        return ResponseEntity.ok(respuesta);
+    }
+    
+}
---
+++ src/main/java/ar/com/ada/api/aladas/models/response/GenericResponse.java
@@ -0,0 +1,10 @@
+package ar.com.ada.api.aladas.models.response;
+
+public class GenericResponse {
+    
+    public boolean isOk;
+
+    public String message;
+
+    public Integer Id;
+}
