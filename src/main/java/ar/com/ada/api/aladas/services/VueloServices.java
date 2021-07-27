7c34d6509632a2de2cfa335f2309ce149ce0c4cf
sami1793 <sami_17_94@hotmail.com>
2021-07-27 17:55:57 -0300

    Se crearon validaciones para el Vuelo. Se uso el enumerado ValidacionVueloDataEnum con todos los tipos de errores. Se hizo unicamente el método para validar el precio. Falta el resto.

--- src/main/java/ar/com/ada/api/aladas/services/VueloServices.java
+++ src/main/java/ar/com/ada/api/aladas/services/VueloServices.java
@@ -44,4 +44,35 @@ public class VueloServices {
 
         return repo.save(vuelo);//puedo poner asi porque devuelve un Vuelo
     }
+
+    public ValidacionVueloDataEnum validar(Vuelo vuelo){
+
+        if(!validarPrecio(vuelo))//si se cumple hay un error en el precio
+            return ValidacionVueloDataEnum.ERROR_PRECIO;
+
+        if(!validarAeropuertoOrigenDiffDestino(vuelo))
+            return ValidacionVueloDataEnum.ERROR_AEROPUERTOS_IGUALES;
+
+        
+        return ValidacionVueloDataEnum.OK;
+    }
+
+    public boolean validarPrecio(Vuelo vuelo){
+        if(vuelo.getPrecio()==null)
+            return false;
+        if(vuelo.getPrecio().doubleValue()>0)
+            return true;
+        return false;
+    }
+
+    public boolean validarAeropuertoOrigenDiffDestino(Vuelo vuelo){
+
+        return vuelo.getAeropuertoOrigen()!=vuelo.getAeropuertoDestino();//verifico que sean diferentes
+    }
+
+    public enum ValidacionVueloDataEnum{
+        OK, ERROR_PRECIO, ERROR_AEROPUERTO_ORIGEN, ERROR_AEROPUERTO_DESTINO, ERROR_FECHA,
+        ERROR_AEROPUERTOS_IGUALES,ERROR_MONEDA, ERROR_CAPACIDAD_MINIMA, ERROR_CAPACIDAD_MAXIMA, 
+        ERROR_GENERAL,
+    }
 }
--- src/test/java/ar/com/ada/api/aladas/AladasApplicationTests.java
+++ src/test/java/ar/com/ada/api/aladas/AladasApplicationTests.java
@@ -1,13 +1,43 @@
 package ar.com.ada.api.aladas;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import ar.com.ada.api.aladas.entities.Vuelo;
+import ar.com.ada.api.aladas.services.VueloServices;
 
 @SpringBootTest
 class AladasApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
+	}*/
+	@Autowired
+	VueloServices vueloServices;
+
+	@Test
+	void vueloTestPrecioNegativo(){
+
+		Vuelo vueloPrecioNegativo = new Vuelo();
+		vueloPrecioNegativo.setPrecio(new BigDecimal(-100));
+		//ASSERT: afirmar
+		//assertTrue: esperamos que sea verdadero
+		//assertFalse: esperamos que sea falso
+		assertFalse(vueloServices.validarPrecio(vueloPrecioNegativo));//si es verdadero el test es correcto
+
+	}
+
+	@Test
+	void vueloTestPrecioOk(){
+		Vuelo vueloPrecioOk = new Vuelo();
+		vueloPrecioOk.setPrecio(new BigDecimal(100));
+
+		assertTrue(vueloServices.validarPrecio(vueloPrecioOk));
 	}
 
 }
