c743e155b2c0215d546599573d14d7e95e964fbf
sami1793 <sami_17_94@hotmail.com>
2021-08-14 21:40:38 -0300

    Se agrego una segunda version de "api/reservasV2" que permite al STAFF crear reservas

--- src/main/java/ar/com/ada/api/aladas/controllers/ReservaController.java
+++ src/main/java/ar/com/ada/api/aladas/controllers/ReservaController.java
@@ -7,7 +7,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import ar.com.ada.api.aladas.entities.Usuario;
+import ar.com.ada.api.aladas.entities.Usuario.TipoUsuarioEnum;
 import ar.com.ada.api.aladas.models.request.InfoReservaNueva;
+import ar.com.ada.api.aladas.models.request.InfoReservaNuevaV2;
 import ar.com.ada.api.aladas.models.response.GenericResponse;
 import ar.com.ada.api.aladas.services.ReservaServices;
 import ar.com.ada.api.aladas.services.UsuarioService;
@@ -38,5 +40,33 @@ public class ReservaController {
         respuesta.message="Reserva creada con éxito";
 
         return ResponseEntity.ok(respuesta);
+    }
+
+    @PostMapping("/api/reservasV2")
+    public ResponseEntity<GenericResponse> generarReservaV2(@RequestBody InfoReservaNuevaV2 infoReservaV2){
+        GenericResponse respuesta = new GenericResponse();
+        //obtengo a quien esta autenticado del otro lado
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        //de lo que esta autenticado obtengo su username
+        String username = authentication.getName();
+
+        Usuario usuario= usuarioService.buscarPorUsername(username);
+
+        Integer numeroReserva;
+
+        if(usuario.getTipoUsuarioId()== TipoUsuarioEnum.PASAJERO){
+
+            numeroReserva = services.generarReserva(infoReservaV2.vueloId, usuario.getPasajero().getPasajeroId());
+        }
+        else
+            numeroReserva= services.generarReserva(infoReservaV2.vueloId, infoReservaV2.pasajeroId);
+
+        respuesta.Id=numeroReserva;
+        respuesta.isOk=true;
+        respuesta.message="Reserva creada con éxito";
+
+        return ResponseEntity.ok(respuesta);
+
+
     }
 }
---
+++ src/main/java/ar/com/ada/api/aladas/models/request/InfoReservaNuevaV2.java
@@ -0,0 +1,6 @@
+package ar.com.ada.api.aladas.models.request;
+
+public class InfoReservaNuevaV2 {
+    public Integer vueloId;
+    public Integer pasajeroId;
+}
